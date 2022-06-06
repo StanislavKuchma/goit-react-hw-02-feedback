2332fa1bd4faa988bbb43a2c79c988882d061cd6
StanislavKuchma <kuchmastanislav73@gmail.com>
2022-06-06 22:17:40 +0300

    html

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,23 @@ export const App = () => {
     <div
       style={{
         height: '100vh',
-        display: 'flex',
+        // display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      React 
+      <p>Please leave feedback</p> 
+      <button class="button">Good</button>
+      <button class="button">Neutral</button>
+       <button class="button">Bad</button>
+      <p>Statistics</p>
+      <div>Good: 4</div>
+      <div>Neutral: 3</div>
+      <div>Bad: 2</div>
+      <div>Total: 1</div>
+      <div>Positive feedback: 43%</div>
     </div>
   );
 };
